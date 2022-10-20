67864874ea2f75a8da0fa52545710e52e9b86dd4
Jonas Driessen <30167@ma-web.nl>
2022-10-20 11:56:09 +0200

    eerste vraag toegevoegd

--- ukraine.py
+++ ukraine.py
@@ -1,3 +1,20 @@
+def q1():
+    print("A. Run as far away from any buildings as you can")
+    print("B. Look for shelter")      
+    awnser = input("")
+    while True:
+        if awnser == "A":
+            print("You roughly guess where the missile is going and you run as far away from it as possible")
+            input("Press enter to continue:")
+        elif awnser == "B":
+            print("You run to the nearest building and many other people do aswell once inside you see a table and you decide "
+            "to hide under it")
+            input("Press enter to continue:")
+        else:
+            print(awnser + " isnt a valid awnser")
+        break
+
+
 print("██╗░░░██╗██╗░░██╗██████╗░░█████╗░██╗███╗░░██╗███████╗  ███████╗░█████╗░██████╗░███████╗██╗░██████╗░███╗░░██╗" "\n"
 "██║░░░██║██║░██╔╝██╔══██╗██╔══██╗██║████╗░██║██╔════╝  ██╔════╝██╔══██╗██╔══██╗██╔════╝██║██╔════╝░████╗░██║" "\n"
 "██║░░░██║█████═╝░██████╔╝███████║██║██╔██╗██║█████╗░░  █████╗░░██║░░██║██████╔╝█████╗░░██║██║░░██╗░██╔██╗██║" "\n"
@@ -11,4 +28,18 @@ print("██╗░░░██╗██╗░░██╗██████╗�
 "███████╗███████╗╚██████╔╝██║╚█████╔╝██║░╚███║" "\n"
 "╚══════╝╚══════╝░╚═════╝░╚═╝░╚════╝░╚═╝░░╚══╝" "\n")
 input("Pres enter to start:")
-print("bruh")
+name = input("Hello, please enter your name:")
+print("Welcome "+name+", This story is about a fighter joining the ukraine foreign legion and going on special missions to help gain intel for the ukrainian militar. You will be playing as him throughout his entire journey and you will be making various choises that and into different outcomes so choose wisely.")
+input("press enter to continue:")
+print("[Arrival]")
+print("As you step out of the train you can hear the announcer from the station say something in polish that you dont understand. "
+"Youve arrived in the polish city of 'Rzeszów' from here on out you will be heading to the ukrainian border crossing."
+"You step out of the station and a taxi was already waiting for you outside of the station, you step into the taxi and are "
+"greeted by a man with a heavy slavic accent Welcome to poland my friend! I assume youre heading to border? he says in a friendly manner. "
+"You nod and he starts driving, he doesnt says much for the rest of the drive and as you come closer to the border you see the traffic increase. "
+"Many ukrainians are still trying to flee the country, you arrive at the border and you're suprized at how easy it is to get into ukraine but not out of it."
+"Once you get through the border you can see military personel at a building and you start heading towards them "
+"when all of a sudden you hear a faint rumbling noise, it almost sounds like a jet engine but then you see it. Its a missile and it appears to be headed"
+"straight towards your location what do you do?")
+input("Press enter to continue:")
+q1()
